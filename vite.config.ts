7cdb87d0b7b1a8f20ba2b7d99d2305697abdbbe1
Arda Kozan <arda.kzn@gmail.com>
2026-01-13 19:49:57 +0300

    edited base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    base: env.VITE_BASE_PATH || '/',
-    plugins: [react()],
-    optimizeDeps: {
-      exclude: ['lucide-react'],
-    },
-  };
+export default defineConfig({
+  base: '/portfolio-arda/',
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
